55c1df3c708aa23a1b00397e9bc5cacb580ef460
shelsql <shelsql@163.com>
2023-12-26 10:39:05 -0800

    ResNet Implemented

---
+++ main.py
@@ -0,0 +1,203 @@
+import torch
+import torch.nn.functional as F
+from models import VGG, ResNet, ResNext
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from tensorboardX import SummaryWriter
+
+from tqdm import tqdm
+import time
+
+
+def train(model, args, run_name):
+    '''
+    Model training function
+    input: 
+        model: linear classifier or full-connected neural network classifier
+        args: configuration
+    '''
+    # create dataset, data augmentation
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    writer = SummaryWriter("logs/" + run_name)
+    train_transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomVerticalFlip(p=0.5),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    test_transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    batch_size = args.batch_size
+    trainset = datasets.CIFAR10(root='./../data', train=True,
+                                            download=True, transform=train_transform)
+    testset = datasets.CIFAR10(root='./../data', train=False,
+                                       download=True, transform=test_transform)
+    # create dataloader
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                            shuffle=True, num_workers=2)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                            shuffle=False, num_workers=2)
+    
+    # create optimizer, scheduler
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr, weight_decay=1e-6)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, args.lr, args.epochs * (len(trainloader) // batch_size))
+    criterion = torch.nn.CrossEntropyLoss()
+
+    # for-loop 
+    model.train()
+    global_step = 0
+    for epoch in range(args.epochs):
+        iterloader = iter(trainloader)
+        print("Epoch %d training..." % (epoch + 1))
+        total_loss = 0
+        total_batches = 0
+        for data in tqdm(iterloader):
+            global_step += 1
+            start_time = time.time()
+            inputs, labels = data
+            B, H, W, C = inputs.shape
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            # get the inputs; data is a list of [inputs, labels]
+            optimizer.zero_grad()
+            # zero the parameter gradients
+            outputs = model(inputs)
+            # forward
+            loss = criterion(outputs, labels)
+            loss.backward()
+            total_loss += loss.item()
+            total_batches += 1
+            writer.add_scalar("Loss/train_step", loss.item(), global_step)
+            # loss backward
+            optimizer.step()
+            # optimize
+            scheduler.step()
+        avg_loss = total_loss / total_batches
+        writer.add_scalar("Loss/train_epoch", avg_loss, epoch + 1)
+        print("Epoch %d Average Loss: %.3f" % (epoch + 1, avg_loss))
+        iterloader = iter(testloader)
+        total = 0
+        correct = 0
+        model.eval()
+        print("Epoch %d testing..." % (epoch + 1))
+        total_loss = 0
+        total_batches = 0
+        with torch.no_grad():
+            for data in tqdm(iterloader):
+                inputs, labels = data
+                B, H, W, C = inputs.shape
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                total_loss += loss.item()
+                total_batches += 1
+                _, predicted = torch.max(outputs, 1)
+                total += labels.shape[0]
+                correct += (predicted == labels).sum().item()
+        acc = 100.0 * correct / total
+        avg_loss = total_loss / total_batches
+        print("Epoch %d Test Average Loss: %.3f" % (epoch + 1, avg_loss))
+        print("Epoch %d Test Accuracy: %.2f %%" % (epoch + 1, acc))
+        writer.add_scalar("Acc/epoch", acc, epoch + 1)
+        writer.add_scalar("Loss/test_epoch", avg_loss, epoch + 1)
+    torch.save({
+            'epoch': args.epochs,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'scheduler_state_dict': scheduler.state_dict()
+            }, "checkpoint.pt")
+
+def test(model, args):
+    '''
+    input: 
+        model: linear classifier or full-connected neural network classifier
+        args: configuration
+    '''
+    # load checkpoint (Tutorial: https://pytorch.org/tutorials/recipes/recipes/saving_and_loading_a_general_checkpoint.html)
+    # create testing dataset
+    # create dataloader
+    # test
+        # forwardif load == True:
+    if args.ckpt_dir != "" and args.run == "test":
+        checkpoint = torch.load(args.ckpt_dir + "/checkpoint.pt")
+        model.load_state_dict(checkpoint['model_state_dict'])
+    # create testing dataset
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    batch_size = 16
+    testset = datasets.CIFAR10(root='./../data', train=False,
+                                       download=True, transform=transform)
+    # create dataloader
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                            shuffle=False, num_workers=2)
+    
+    criterion = torch.nn.CrossEntropyLoss()
+    # test
+        # forward
+    iterloader = iter(testloader)
+    total = 0
+    correct = 0
+    total_loss = 0
+    total_batches = 0
+    model.eval()
+    print("Testing...")
+    for data in tqdm(iterloader):
+        inputs, labels = data
+        B, H, W, C = inputs.shape
+        inputs = inputs.reshape(B, H*W*C).to(device)
+        labels = labels.to(device)
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        total_loss += loss.item()
+        total_batches += 1
+        _, predicted = torch.max(outputs, 1)
+        total += labels.shape[0]
+        correct += (predicted == labels).sum().item()
+    avg_loss = total_loss / total_batches
+    acc = 100 * correct / total
+    print("Test Average Loss: %.3f" % (avg_loss))
+    print("Test Accuracy: %.2f %%"%(acc))
+    return acc, avg_loss
+
+if __name__ == '__main__':
+    import argparse
+    
+    parser = argparse.ArgumentParser(description='The configs')
+
+    parser.add_argument('--run', type=str, default='train')
+    parser.add_argument('--model', type=str, default='vgg')
+    parser.add_argument('--norm', type=str, default='batch')
+    parser.add_argument('--dropout', type=float, default=0.5)
+    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument("--lr", type=float, default=1e-4)
+    parser.add_argument("--batch_size", type=int, default=16)
+    parser.add_argument("--ckpt_dir", type=str, default="")
+    args = parser.parse_args()
+
+    run_name = args.run + "_" + args.model + "_" + args.norm + "_" + str(args.dropout) + "_" + str(args.lr)
+    import datetime
+    run_date = datetime.datetime.now().strftime('%H%M%S')
+    run_name = run_name + '_' + run_date
+    
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    if args.model == "vgg":
+        model = VGG(dropout=args.dropout).to(device)
+    elif args.model == "resnet":
+        model = ResNet().to(device)
+    elif args.model == "resnext":
+        model = ResNext().to(device)
+    else: 
+        raise AssertionError
+    
+    
+    if args.run == 'train':
+        train(model, args, run_name)
+    elif args.run == 'test':
+        test(model, args)
+    else: 
+        raise AssertionError
+    
+    # train / test
---
+++ models.py
@@ -0,0 +1,174 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class VGG(nn.Module):
+    def __init__(self, in_height=32, in_width=32, in_channels=3, dropout=0.5):
+        super().__init__()
+        print("VGG16 initializing...")
+        # 1. define multiple convolution and downsampling layers
+        # 3. define full-connected layer to classify
+        self.config = [64, 64, "M", 128, 128, "M", 256, 256, 256, "M", 512, 512, 512, "M", 512, 512, 512, "M"]
+        convs = []
+        for layer in self.config:
+            if type(layer) == int:
+                out_channels = layer
+                convs.append(nn.Conv2d(in_channels, out_channels, 3, 1, 1))
+                convs.append(nn.BatchNorm2d(out_channels))
+                convs.append(nn.ReLU())
+                in_channels = out_channels
+            elif layer == "M":
+                convs.append(nn.MaxPool2d(2, 2))
+        self.convs = nn.Sequential(*convs)
+        self.fc = nn.Sequential(
+            nn.Linear(out_channels, 4096),
+            nn.ReLU(),
+            nn.Dropout(p = dropout),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Dropout(p = dropout),
+            nn.Linear(4096, 10)
+        )
+                
+
+    def forward(self, x: torch.Tensor):
+        # x: input image, shape: [B * C * H* W]
+        # extract features
+        # classification
+        out = self.convs(x)
+        out = out.reshape(x.shape[0], -1)
+        out = self.fc(out)
+        return out
+
+
+class ResBlock(nn.Module):
+    ''' residual block'''
+    def __init__(self, in_channels, out_channels, stride, downsample = None):
+        super().__init__()
+        '''
+        in_channel: number of channels in the input image.
+        out_channel: number of channels produced by the convolution.
+        stride: stride of the convolution.
+        '''
+        # 1. define double convolution
+             # convolution
+             # batch normalization
+             # activate function
+             # ......
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, stride, 1),
+            nn.BatchNorm2d(out_channels)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1),
+            nn.BatchNorm2d(out_channels)
+        )
+        self.relu = nn.ReLU()
+        self.use_conv3 = in_channels != out_channels or stride != 1
+        # 2. if in_channel != out_channel or stride != 1, deifine 1x1 convolution layer to change the channel or size.
+        if self.use_conv3:
+            self.conv3 = nn.Conv2d(in_channels, out_channels, 1, stride)
+        # Note: we are going to implement 'Basic residual block' by above steps, you can also implement 'Bottleneck Residual block'
+
+    def forward(self, x: torch.Tensor):
+        # x: input image, shape: [B * C * H* W]
+        # 1. convolve the input
+        out = self.conv1(x)
+        out = self.relu(out)
+        out = self.conv2(out)
+        # 2. if in_channel != out_channel or stride != 1, change the channel or size of 'x' using 1x1 convolution.
+        if self.use_conv3:
+            x = self.conv3(x)
+        # 3. Add the output of the convolution and the original data (or from 2.)
+        out = out + x
+        # 4. relu
+        out = self.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    '''residual network'''
+    def __init__(self):
+        super().__init__()
+        print("ResNet18 initializing...")
+        self.config = [(2, 64, False), (2, 128, True), (2, 256, True), (2, 512, True)]
+        # 1. define convolution layer to process raw RGB image
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, 7, 2, 3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(3, 2, 1)
+        )
+        # 2. define multiple residual blocks
+        blocks = []
+        in_channels = 64
+        for num, channels, down in self.config:
+            block = []
+            for i in range(num):
+                out_channels = channels
+                stride = 2 if i == 0 and down else 1
+                block.append(ResBlock(in_channels, out_channels, stride))
+                in_channels = out_channels
+            blocks.append(nn.Sequential(*block))
+        self.blocks = nn.Sequential(*blocks)
+        # 3. define full-connected layer to classify
+        self.maxpool = nn.AdaptiveAvgPool2d(1)
+        self.linear = nn.Linear(512, 10)
+
+    def forward(self, x: torch.Tensor):
+        # x: input image, shape: [B * C * H* W]
+        # extract features
+        # classification
+        out = self.conv1(x)
+        out = self.blocks(out)
+        out = self.maxpool(out)
+        out = out.reshape(x.shape[0], -1)
+        out = self.linear(out)
+        return out
+    
+
+class ResNextBlock(nn.Module):
+    '''ResNext block'''
+    def __init__(self, in_channel, out_channel, bottle_neck, group, stride):
+        super().__init__()
+        print("ResNext initializing...")
+        # in_channel: number of channels in the input image
+        # out_channel: number of channels produced by the convolution
+        # bottle_neck: int, bottleneck= out_channel / hidden_channel 
+        # group: number of blocked connections from input channels to output channels
+        # stride: stride of the convolution.
+
+        # 1. define convolution
+             # 1x1 convolution
+             # batch normalization
+             # activate function
+             # 3x3 convolution
+             # ......
+             # 1x1 convolution
+             # ......
+
+        # 2. if in_channel != out_channel or stride != 1, deifine 1x1 convolution layer to change the channel or size.
+
+    def forward(self, x: torch.Tensor):
+        # x: input image, shape: [B * C * H* W]
+        # 1. convolve the input
+        # 2. if in_channel != out_channel or stride != 1, change the channel or size of 'x' using 1x1 convolution.
+        # 3. Add the output of the convolution and the original data (or from 2.)
+        # 4. relu
+        return out
+
+
+class ResNext(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # 1. define convolution layer to process raw RGB image
+        # 2. define multiple residual blocks
+        # 3. define full-connected layer to classify
+
+    def forward(self, x: torch.Tensor):
+        # x: input image, shape: [B * C * H* W]
+        # extract features
+        # classification
+        return out
+
